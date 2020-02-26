10a70525ed075a380d0b162a2fc4e1bf687a0f28
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-02-26 17:29:02 +0100

    Added first implementation of UNet.

---
+++ qprof/models/unet/pytorch.py
@@ -0,0 +1,100 @@
+import torch
+from torch import nn
+
+class Block(nn.Sequential):
+    def __init__(self,
+                 in_features,
+                 out_features,
+                 depth=2,
+                 activation=nn.ReLU(),
+                 no_activation = False,
+                 mode="none"):
+
+        # First layer, increase number of features.
+        if depth == 1:
+            if no_activation:
+                modules = [nn.ConstantPad2d(1, -1.0),
+                           nn.Conv2d(in_features, out_features, 3)]
+            else:
+                modules = [nn.ConstantPad2d(1, -1.0),
+                           nn.Conv2d(in_features, out_features, 3),
+                           activation]
+        else:
+            modules = [nn.ConstantPad2d(1, -1.0),
+                       nn.Conv2d(out_features, out_features, 3),
+                       activation]
+            if no_activation:
+                modules += [nn.ConstantPad2d(1, -1.0),
+                            nn.Conv2d(out_features, out_features, 3),
+                            activation] * max(depth - 2, 0)
+                modules += [nn.ConstantPad2d(1, -1.0),
+                            nn.Conv2d(out_features, out_features, 3)]
+            else:
+                modules += [nn.ConstantPad2d(1, -1.0),
+                            nn.Conv2d(out_features, out_features, 3),
+                            activation] * max(depth - 1, 0)
+
+        super().__init__(*modules)
+
+class DownSampler(nn.Sequential):
+    def __init__(self):
+        modules = [nn.MaxPool2d(2)]
+        super().__init__(*modules)
+
+class UpSampler(nn.Sequential):
+    def __init__(self,
+                 features_in,
+                 features_out):
+        modules = [nn.ConstantPad2d(1, -1.0),
+                   nn.Conv2d(features_in, features_out, 3)]
+        super().__init__(*modules)
+
+class UNet(nn.Module):
+    def __init__(self,
+                 input_features,
+                 output_features,
+                 n_features=32,
+                 n_levels=4):
+
+        super().__init__()
+
+        # Down-sampling blocks
+        self.down_blocks = nn.ModuleList()
+        self.down_samplers = nn.ModuleList()
+        features_in = input_features
+        features_out = n_features
+        for i in range(n_levels - 1):
+            self.down_blocks.append(Block(features_in, features_out))
+            self.down_samplers.append(DownSampler())
+            features_in = features_out
+            features_out = features_out * 2
+
+        self.center_block = Block(features_in, features_out)
+
+        self.up_blocks = nn.ModuleList()
+        self.up_samplers = nn.ModuleList()
+        features_in = features_out
+        for i in range(n_levels - 1):
+            self.up_samplers.append(UpSampler(features_in, features_in // 2))
+            self.up_blocks.append(Block(features_in, features_in // 2))
+            features_in = features_in // 2
+
+        self.head = nn.Conv2d(features_in, output_features, 3)
+
+
+        def __forward__(self, x):
+
+            features = []
+            for (b, s) in zip(self.down_blocks, self.down_samplers):
+                x = b(x)
+                features.append(x)
+                x = s(x)
+
+            x = self.center_block(x)
+
+            for (b, u, f) in zip(self.up_blocks, self.up_samplers, reverse(features)):
+                x = u(x)
+                x = torch.cat([x, f], 1)
+                x = b(x)
+
+            return self.head(x)
